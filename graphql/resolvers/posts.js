47214f519b73c7d4a3451061b0a0d87e588ed923
Igor Melo <igorjmelo4@gmail.com>
2019-07-13 19:26:22 -0300

    adding Subscriptions and like/comment counters

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -39,6 +39,10 @@ module.exports = {
 
             const post = await newPost.save()
 
+            context.pubsub.publish('NEW_POST', {
+                newPost: post
+            })
+
             return post
         },
         async deletePost(_, { postId }, context) {
@@ -80,6 +84,11 @@ module.exports = {
             } catch (error) {
                 throw new Error(error)
             }
+        }
+    },
+    Subscription: {
+        newPost: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
         }
     }
 }
