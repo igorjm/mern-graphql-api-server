99a39a74c34c19d553105615a831f9edbf819309
igor.melo <igor.melo@ilog.com.br>
2019-11-26 23:41:42 -0300

    adding config

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://igorjm:QkbJLkmcgEEGrck6@mern-graphql-api-0ska4.mongodb.net/test?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
